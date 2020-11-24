18c0c8ba5ecabc9ab6c8ca7df65f7ae005de6527
Shadowru <proteyrus@gmail.com>
2020-11-24 21:47:40 +0300

    Video

--- web_interface/public/assets/js/z_rosbot_UI.js
+++ web_interface/public/assets/js/z_rosbot_UI.js
@@ -198,6 +198,8 @@ window.onload = function () {
 		setView()
 	};
 
+
+	document.getElementById('video-container').src = "http://" + robot_host + ":8082/stream?topic=/image_raw&type=mjpeg&quality=100";
 	document.getElementById('map-video').src = "http://" + robot_host + ":8082/stream?topic=/map_image/tile&type=mjpeg&quality=100";
 
 	mapZoomSlider = document.getElementById("map-zoom");
